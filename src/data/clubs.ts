e19076ca962a8d91630f538de804a5b265a1a17f
cardiffjitsuclub <cardiffjitsuclub@gmail.com>
2026-09-18 15:27:13 +0100

    Add Wales home page and Cardiff City club page

---
+++ src/data/clubs.ts
@@ -0,0 +1,8 @@
+export const clubs = [
+  { name: 'Cardiff City', href: '/clubs/cardiff/' },
+  { name: 'Cardiff Metropolitan University' },
+  { name: 'Cardiff University' },
+  { name: 'Swansea University' },
+  { name: 'St. Athan' },
+  { name: 'Vale' },
+];
